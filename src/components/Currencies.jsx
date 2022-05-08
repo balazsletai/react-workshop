41d9c0ec198519e84433e700849698ab2b6beb0d
Balazs Letai <balazs.letai@liferay.com>
2022-05-08 17:24:31 +0200

    changed design

--- src/components/Currencies.jsx
+++ src/components/Currencies.jsx
@@ -8,6 +8,7 @@ export default function Currency({ name, value, onChange, readOnly }) {
   }
 
   const elementToShow = (
+    <div className='container'>
       <label className='currency'>
         <strong>{name}</strong>
         <br />
@@ -18,9 +19,10 @@ export default function Currency({ name, value, onChange, readOnly }) {
           readOnly={readOnly}
           min='0'
           placeholder='0'
-          step='0.001'
+          step='0.01'
         />
       </label>
+    </div>
   )
 
   return (elementToShow);
