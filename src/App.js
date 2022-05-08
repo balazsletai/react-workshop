7cbbecc3ba9463f4990f92bb9fadf0be9b5766c7
Balazs Letai <balazs.letai@liferay.com>
2022-05-08 13:15:54 +0200

    added BTC and conversion calculation, set timeout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
-import React, { useState } from 'react';
-import Currency from './components/currency.jsx';
+import React, { useState, useEffect } from 'react';
+import Currency from './components/Currency';
 
 function App() {
+
   const [amount, setAmount] = useState(0);
+  const [conversionRate, setConversionRate] = useState(rateCalculation);
+
+  function rateCalculation() {
+    return Math.random() * 0.01;
+  }
+
+  useEffect(() => {
+    let timer = setTimeout(() => { setConversionRate(0) }, 5000);
+
+    return () => { clearTimeout(timer) }
+
+  }, [amount]);
+
+  function onChange(amount) {
+    setAmount(amount);
+
+    if (conversionRate === 0) { setConversionRate(rateCalculation); }
+
+  }
 
   return (
 
     <div className='App'>
-      <Currency value={amount} onChange={setAmount} />
+      <Currency name='Euros' value={amount} onChange={onChange} />
+      <br />
+      <br />
+      <Currency name='BTC' value={parseFloat(amount * conversionRate).toFixed(6)} readOnly />
     </div>);
 
 };
--- src/components/currency.jsx
+++ src/components/currency.jsx
@@ -1,27 +1,26 @@
 import './currency.css';
 import React from 'react';
 
-export default function Currency({ value, onChange }) {
+export default function Currency({ name, value, onChange, readOnly }) {
 
   function onChangeHandler(event) {
     onChange(event.currentTarget.value);
   }
 
   const elementToShow = (
-    <>
-      <label htmlFor='currency'><strong>Euros</strong></label>
-      <br />
-      <input
-        onChange={onChangeHandler}
-        value={value}
-        type='number'
-        className='currency'
-        id='currency'
-        min='0'
-        placeholder='0'
-        step='0.001'
-      />
-    </>
+      <label className='currency'>
+        <strong>{name}</strong>
+        <br />
+        <input
+          onChange={onChangeHandler}
+          value={value}
+          type='number'
+          readOnly={readOnly}
+          min='0'
+          placeholder='0'
+          step='0.001'
+        />
+      </label>
   )
 
   return (elementToShow);
