1f23629a23a41e9c554e8219bb83afdd2682cbb8
Balazs Letai <balazs.letai@liferay.com>
2022-06-23 18:13:20 +0200

    week5 homework with separate components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,21 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import Currency from './components/Currencies';
-import ThemeContext from './ThemeContext';
+import { useState } from 'react';
+import CardList from './components/CardList';
+import Form from './components/Form';
 
-function App() {
+const App = () => {
+  const [cards, setCards] = useState([])
 
-  const [amount, setAmount] = useState(0);
-  const [conversionRate, setConversionRate] = useState(rateCalculation);
-  const [theme, setTheme] = useState('light');
-
-  function rateCalculation() {
-    return Math.random() * 0.01;
-  }
-
-  useEffect(() => {
-    let timer = setTimeout(() => { setConversionRate(0) }, 5000);
-
-    return () => { clearTimeout(timer) }
-
-  }, [amount]);
-
-  function onChangeOfAmount(amount) {
-    setAmount(amount);
-
-    if (conversionRate === 0) { setConversionRate(rateCalculation); }
-
-  }
-
-  function onChangeOfTheme(event) {
-    setTheme(event.target.value);
+  const addNewCard = cardInfo => {
+    setCards(cards.concat(cardInfo))
   }
 
   return (
-    <ThemeContext.Provider value={{ storeTheme: theme }} >
-      <div className={`App ${theme}`}>
-        <Currency name='Euros' value={amount} onChange={onChangeOfAmount} />
-        <br />
-        <br />
-        <Currency name='BTC' value={parseFloat(amount * conversionRate).toFixed(6)} readOnly />
-        <br />
-        <label className='theme'>
-          <br />
-          <strong>Select Theme</strong>
-          <br />
-          <select
-            onChange={onChangeOfTheme}
-            value={theme}
-          >
-            <option value='dark'>Dark</option>
-            <option value='light'>Light</option>
-          </select>
-        </label>
-      </div>
-    </ThemeContext.Provider >
-  );
-};
-
+    <div>
+      <Form onSubmit={addNewCard} />
+      <CardList cards={cards} />
+    </div>
+  )
+}
 
 export default App;
